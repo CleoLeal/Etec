5699e04d0d9f850625453f1e72dee5205ff4080b
Cleo Leal <94088457+CleoLeal@users.noreply.github.com>
2022-08-18 17:01:57 -0300

    Banco de dados
    Arquivo para controlar o banco de dados em PHP

---
+++ Programacao Web III/salvar.php
@@ -0,0 +1,12 @@
+<?php
+    $email=$_POST["email"];
+    $senha1=$_POST["senha1"];
+    $senha2=$_POST["senha2"];
+    $nome=$_POST["nome"];
+    $dtnc=$_POST["dtnc"];
+
+    if($senha1==$senha2)
+    {
+        
+    }
+?>
