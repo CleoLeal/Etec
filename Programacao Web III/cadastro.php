4913c7648fee0fdcc2b7f376ac4a88701839d9ec
Cleo Leal <94088457+CleoLeal@users.noreply.github.com>
2022-08-18 16:58:56 -0300

    Utilização de Div
    Antes eu estava usando table, mas preferi alterar para div com o CSS externo, achei mais simples de mexer

--- Programacao Web III/cadastro.php
+++ Programacao Web III/cadastro.php
@@ -1,37 +1,31 @@
 <!DOCTYPE html>
-    <html lang="en">
+    <html lang="pt-br">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Página de cadastro</title>
     </head>
-        <link rel="stylesheet" type="text/css" href="syyle.css" media="screen">
+        <link rel="stylesheet" type="text/css" href="style.css" media="screen">
     <body>
         <form accept-charset="UTF-8" action="salvar.php" autocomplete="off" method="POST">
-            <table border="0">
-            <tr>
-                <td>E-mail:</td>
-                <td><input type="email" name="email" id="email" require></td>
-            </tr>  
-            <tr>
-                <td>Senha:</td>
-                <td><input type="password" name="senha1" id="senha1"  required parttern="^(?=,*[a-z])(?=,*[A-Z])(?=,*[0-9]).{3,}$"></td>
-            </tr>
-            <tr>
-                <td>Confirmação de senha:</td>
-                <td><input type="password" name="senha2" id="senha2"  required parttern="^(?=,*[a-z])(?=,*[A-Z])(?=,*[0-9]).{3,}$"></td>
-            </tr>
-            <tr>
-                <td>Nome:</td>
-                <td><input type="text" name="nome" id="nome"></td>
-            </tr>
-            <tr>
-                <td>Data de nascimento:</td>
-                <td><input type="date" name="data" id="data"></td>
-            </table>
-<br>   
+            <div class="font">E-mail:</div>
+            <div class="input"><input type="email" name="email" maxlength="40"  require></div>
+<br>
+            <div class="font">Senha:</div>
+            <div class="input"><input type="password" name="senha1" id="senha1" maxlength="40" required parttern="^(?=,*[a-z])(?=,*[A-Z])(?=,*[0-9]).{3,}$"></div>
+<br>
+            <div class="font">Confirmação de senha:</div>
+            <div class="input"><input type="password" name="senha2" id="senha2" maxlength="40"></div>
+<br>
+            <div class="font">Nome:</div>
+            <div class="input"><input type="nome" name="nome" maxlength="40"></div>
+<br>
+            <div class="font">Data de nascimento:</div>
+            <div class="input"><input type="dtnc" name="dtnc" maxlength="40"></div>
+<br>
             <button type="submit" name="enviar" id="enviar" class="botao">Enviar</button>
+            <button type="reset" name="limpar" id="limpar" class="botao">Limpar</button>
         </form>
     </body>
 </html>
